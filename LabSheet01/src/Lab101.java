a75f5a2c72994158ff86b583e881317b6fd55f9c
koonkao.kr@gmail.com <koonkao.kr@gmail.com>
2025-11-18 14:22:11 +0700

    KittithongLabSheet01

--- LabSheet01/src/Lab101.java
+++ LabSheet01/src/Lab101.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("Hello,I am Kittithong Rachasanesinghdacha");
+        //System.out.println("\"TNI-108\" Object-Oriented Programming");
+        System.out.println("\tThis is my frist time learning java");
+        System.out.println("I hope to get grade \"A\"");
+    }
 }
--- LabSheet01/src/Lab102.java
+++ LabSheet01/src/Lab102.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Lab102 {
+    public static void main(String[] args) {
+        System.out.println("Summation NUmber = " + 2 + 5);
+    }
 }
